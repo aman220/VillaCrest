0a5c7882f8b99d3a5f336182cc626f6a223bc78c
aman220 <aman.singh_cs22@gla.ac.in>
2024-06-05 14:55:42 +0530

    contact us

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../HomeComponents/navbar/navbar.component';
+import { FooterComponent } from '../HomeComponents/footer/footer.component';
 
 @Component({
   selector: 'app-contactus',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent , FooterComponent],
   templateUrl: './contactus.component.html',
   styleUrl: './contactus.component.css'
 })
